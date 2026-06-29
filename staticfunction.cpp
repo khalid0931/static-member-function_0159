293fd81de1e248a052f426c6572f9b8f35dc32c1
khalid0931 <mutiarif53@gmail.com>
2026-06-29 07:40:50 +0700

    membuat library file

---
+++ staticfunction.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string> //menyertakan library string
+using namespace std;
+
+class Mahasiswa {
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
